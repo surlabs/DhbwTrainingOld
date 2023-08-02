e43ee392393221d104debb00da46d2e1474fe2a0
jcopado <jcopado@surlabs.es>
2023-08-02 15:03:15 +0200

    SURLABS (I) plugin.php

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = "xdht";
-$version = "2.0.4";
-$ilias_min_version = "6.0";
-$ilias_max_version = "7.999";
+$version = "3.0.0";
+$ilias_min_version = "8.0";
+$ilias_max_version = "8.999";
 $responsible = "fluxlabs ag";
 $responsible_mail = "support@fluxlabs.ch";
 $learning_progress = true;
